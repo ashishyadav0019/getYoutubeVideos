97069c2636640bbd6064c271200d607deaf1559f
ashishyadav0019 <ashishyadav0019@gmail.com>
2018-05-01 16:55:41 +0530

    new code

---
+++ src/configuration.js
@@ -0,0 +1,8 @@
+var configuration = {
+    "youtube":{
+        API_KEY:"AIzaSyD_7AWLEIZbeNxQHtjAc_0RqWvdoza2rJo",
+        host_url:"https://www.googleapis.com/youtube/v3/search?part=snippet&",
+        video_url:"https://www.youtube.com/embed/"
+    }
+};
+module.exports = configuration;
